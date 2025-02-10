da5bfc9b8a6fcccc70bf8b54c4418035c3692607
DavidDornelas77 <dornelasdavid22@hotmail.com>
2025-02-10 20:49:32 -0300

    Função-adicionar-nomes pt1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeDosAmigos = [];
+
+function adicionarAmigo() {
+    let amigos = document.getElementById("amigo").value.trim();
+    if (amigos === ""){
+        alert('Por favor, insira um nome.');
+        return;        
+    }
+    nomeDosAmigos.push(amigos);
+    console.log(nomeDosAmigos);
+    document.getElementById("amigo").value = "";
+}
